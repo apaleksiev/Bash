d307eb6308baf712bfae8414559b55e579a9ca5c
apaleksiev <84075409+apaleksiev@users.noreply.github.com>
2022-03-08 14:54:05 +0200

    Terraform resources Naming Convention Changer

---
+++ tf_rename.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#I needed to do a mass naming convention fix on hundreds of resources
+#The third line accomplished what I needed it to do but terraform has varying levels of indentation/tabs
+#which was messing up the script- thus I had to normalize all white spaces and terraform fmt when changed
+
+
+for file in $(find . -name "*.tf");
+do
+sed -i 's/\([^[:space:]]\)[[:space:]]\{1,\}/\1 /g; s/[[:space:]]*$//' "$file"
+sed -i 's/^[ \t]*//' "$file"
+sed -i 's/^\( *name *= *"\)${var.app_name}-${var.app_env}-\(.*\)"/\1\2-${var.app_env}"/i' "$file"
+done
+
+terraform fmt -recursive
+
